a7eb24ccf4f9e40879ada15f369def5a6dee8b8b
lamHaiTrieu <67643448+lhtlhtrieu036@users.noreply.github.com>
2022-06-21 15:55:21 +0700

    Add files via upload
    them benh nhan form

---
+++ BenhNhan_Main.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Test2
+{
+    partial class BenhNhan_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_ThongTinBN = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_CMND_BN = new System.Windows.Forms.TextBox();
+            this.textBox_tenBN = new System.Windows.Forms.TextBox();
+            this.textBox_quanHuyenBN = new System.Windows.Forms.TextBox();
+            this.textBox_tenDuongBN = new System.Windows.Forms.TextBox();
+            this.textBox_tinhThanhPho_BN = new System.Windows.Forms.TextBox();
+            this.textBox_soNhaBN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker_ngaySinhBN = new System.Windows.Forms.DateTimePicker();
+            this.button_capNhatBN = new System.Windows.Forms.Button();
+            this.button_thoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ThongTinBN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_ThongTinBN
+            // 
+            this.dgv_ThongTinBN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ThongTinBN.Location = new System.Drawing.Point(54, 145);
+            this.dgv_ThongTinBN.Name = "dgv_ThongTinBN";
+            this.dgv_ThongTinBN.RowHeadersWidth = 51;
+            this.dgv_ThongTinBN.RowTemplate.Height = 24;
+            this.dgv_ThongTinBN.Size = new System.Drawing.Size(1024, 119);
+            this.dgv_ThongTinBN.TabIndex = 2;
+            this.dgv_ThongTinBN.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_ThongTinBN_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(385, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(357, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "THÔNG TIN BỆNH NHÂN";
+            // 
+            // textBox_CMND_BN
+            // 
+            this.textBox_CMND_BN.Location = new System.Drawing.Point(747, 315);
+            this.textBox_CMND_BN.Name = "textBox_CMND_BN";
+            this.textBox_CMND_BN.Size = new System.Drawing.Size(195, 22);
+            this.textBox_CMND_BN.TabIndex = 5;
+            // 
+            // textBox_tenBN
+            // 
+            this.textBox_tenBN.Location = new System.Drawing.Point(219, 312);
+            this.textBox_tenBN.Name = "textBox_tenBN";
+            this.textBox_tenBN.Size = new System.Drawing.Size(195, 22);
+            this.textBox_tenBN.TabIndex = 6;
+            // 
+            // textBox_quanHuyenBN
+            // 
+            this.textBox_quanHuyenBN.Location = new System.Drawing.Point(747, 429);
+            this.textBox_quanHuyenBN.Name = "textBox_quanHuyenBN";
+            this.textBox_quanHuyenBN.Size = new System.Drawing.Size(195, 22);
+            this.textBox_quanHuyenBN.TabIndex = 7;
+            // 
+            // textBox_tenDuongBN
+            // 
+            this.textBox_tenDuongBN.Location = new System.Drawing.Point(219, 429);
+            this.textBox_tenDuongBN.Name = "textBox_tenDuongBN";
+            this.textBox_tenDuongBN.Size = new System.Drawing.Size(195, 22);
+            this.textBox_tenDuongBN.TabIndex = 8;
+            // 
+            // textBox_tinhThanhPho_BN
+            // 
+            this.textBox_tinhThanhPho_BN.Location = new System.Drawing.Point(219, 488);
+            this.textBox_tinhThanhPho_BN.Name = "textBox_tinhThanhPho_BN";
+            this.textBox_tinhThanhPho_BN.Size = new System.Drawing.Size(195, 22);
+            this.textBox_tinhThanhPho_BN.TabIndex = 9;
+            // 
+            // textBox_soNhaBN
+            // 
+            this.textBox_soNhaBN.Location = new System.Drawing.Point(747, 370);
+            this.textBox_soNhaBN.Name = "textBox_soNhaBN";
+            this.textBox_soNhaBN.Size = new System.Drawing.Size(195, 22);
+            this.textBox_soNhaBN.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Họ và tên bệnh nhân";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(573, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Số chứng minh nhân dân";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Ngày sinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(573, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Số nhà";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Tên đường";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(573, 429);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Quận/Huyện";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 491);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Tỉnh/Thành phố";
+            // 
+            // dateTimePicker_ngaySinhBN
+            // 
+            this.dateTimePicker_ngaySinhBN.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker_ngaySinhBN.Location = new System.Drawing.Point(219, 370);
+            this.dateTimePicker_ngaySinhBN.Name = "dateTimePicker_ngaySinhBN";
+            this.dateTimePicker_ngaySinhBN.Size = new System.Drawing.Size(195, 22);
+            this.dateTimePicker_ngaySinhBN.TabIndex = 19;
+            // 
+            // button_capNhatBN
+            // 
+            this.button_capNhatBN.Location = new System.Drawing.Point(576, 501);
+            this.button_capNhatBN.Name = "button_capNhatBN";
+            this.button_capNhatBN.Size = new System.Drawing.Size(115, 40);
+            this.button_capNhatBN.TabIndex = 20;
+            this.button_capNhatBN.Text = "CẬP NHẬT";
+            this.button_capNhatBN.UseVisualStyleBackColor = true;
+            this.button_capNhatBN.Click += new System.EventHandler(this.button_capNhatBN_Click);
+            // 
+            // button_thoat
+            // 
+            this.button_thoat.Location = new System.Drawing.Point(785, 501);
+            this.button_thoat.Name = "button_thoat";
+            this.button_thoat.Size = new System.Drawing.Size(115, 40);
+            this.button_thoat.TabIndex = 21;
+            this.button_thoat.Text = "THOÁT";
+            this.button_thoat.UseVisualStyleBackColor = true;
+            this.button_thoat.Click += new System.EventHandler(this.button_thoat_Click);
+            // 
+            // BenhNhan_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 605);
+            this.Controls.Add(this.button_thoat);
+            this.Controls.Add(this.button_capNhatBN);
+            this.Controls.Add(this.dateTimePicker_ngaySinhBN);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_soNhaBN);
+            this.Controls.Add(this.textBox_tinhThanhPho_BN);
+            this.Controls.Add(this.textBox_tenDuongBN);
+            this.Controls.Add(this.textBox_quanHuyenBN);
+            this.Controls.Add(this.textBox_tenBN);
+            this.Controls.Add(this.textBox_CMND_BN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv_ThongTinBN);
+            this.Name = "BenhNhan_Main";
+            this.Text = "BenhNhan_Main";
+            this.Load += new System.EventHandler(this.BenhNhan_Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ThongTinBN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_ThongTinBN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_CMND_BN;
+        private System.Windows.Forms.TextBox textBox_tenBN;
+        private System.Windows.Forms.TextBox textBox_quanHuyenBN;
+        private System.Windows.Forms.TextBox textBox_tenDuongBN;
+        private System.Windows.Forms.TextBox textBox_tinhThanhPho_BN;
+        private System.Windows.Forms.TextBox textBox_soNhaBN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_ngaySinhBN;
+        private System.Windows.Forms.Button button_capNhatBN;
+        private System.Windows.Forms.Button button_thoat;
+    }
+}
---
+++ BenhNhan_Main.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Test2
+{
+    public partial class BenhNhan_Main : Form
+    {
+        OracleConnection conn = new OracleConnection(Globals.Data_String);
+        OracleDataAdapter adapter;
+        OracleCommand command;
+        public BenhNhan_Main()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void BenhNhan_Main_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+            string sql = "select * from SUPERADMIN.Benh_Nhan";
+            command = new OracleCommand(sql, conn);
+
+            DataTable data = new DataTable();
+            adapter = new OracleDataAdapter(command);
+            adapter.Fill(data);
+            DataTable tbl = new DataTable();
+            adapter.Fill(tbl);
+            dgv_ThongTinBN.DataSource = tbl;
+            dgv_ThongTinBN.Columns[0].HeaderText = "MÃ BỆNH NHÂN";
+            dgv_ThongTinBN.Columns[1].HeaderText = "CƠ SỞ Y TẾ";
+            dgv_ThongTinBN.Columns[2].HeaderText = "TÊN BỆNH NHÂN";
+            dgv_ThongTinBN.Columns[3].HeaderText = "SỐ CMND";
+            dgv_ThongTinBN.Columns[4].HeaderText = "NGÀY SINH";
+            dgv_ThongTinBN.Columns[5].HeaderText = "SỐ NHÀ";
+            dgv_ThongTinBN.Columns[6].HeaderText = "TÊN ĐƯỜNG";
+            dgv_ThongTinBN.Columns[7].HeaderText = "QUẬN/HUYỆN";
+            dgv_ThongTinBN.Columns[8].HeaderText = "TỈNH/THÀNH PHỐ";
+            dgv_ThongTinBN.Columns[9].HeaderText = "DỰ ĐOÁN";
+            dgv_ThongTinBN.Columns[10].HeaderText = "TIỀN SỬ BỆNH";
+            dgv_ThongTinBN.Columns[11].HeaderText = "DỊ ỨNG THUỐC";
+            dgv_ThongTinBN.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+            //textBox_tenBN.Text = dgv_ThongTinBN.CurrentCell.
+            conn.Close();
+        }
+
+        public void Load_Data()
+        {
+            string sql = "select * from SUPERADMIN.Benh_Nhan";
+            command = new OracleCommand(sql, conn);
+
+            DataTable data = new DataTable();
+            adapter = new OracleDataAdapter(command);
+            adapter.Fill(data);
+            dgv_ThongTinBN.DataSource = data;
+        }
+
+        private void dgv_ThongTinBN_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox_tenBN.Text = dgv_ThongTinBN.CurrentRow.Cells["TEN_BN"].Value.ToString();
+            textBox_CMND_BN.Text = dgv_ThongTinBN.CurrentRow.Cells["CMND"].Value.ToString();
+            textBox_quanHuyenBN.Text = dgv_ThongTinBN.CurrentRow.Cells["QUAN_HUYEN"].Value.ToString();
+            textBox_soNhaBN.Text = dgv_ThongTinBN.CurrentRow.Cells["SO_NHA"].Value.ToString();
+            textBox_tenDuongBN.Text = dgv_ThongTinBN.CurrentRow.Cells["TEN_DUONG"].Value.ToString();
+            textBox_tinhThanhPho_BN.Text = dgv_ThongTinBN.CurrentRow.Cells["TINH_TP"].Value.ToString();
+        }
+
+        private void button_capNhatBN_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string sql = "";
+            sql = @"Update SUPERADMIN.BENH_NHAN set TEN_BN = :Hoten , CMND = :SoCMND , SO_NHA = :sonha , TEN_DUONG = :tenduong , QUAN_HUYEN = :quanhuyen , TINH_TP = :tinhtp where MA_BN = '" + Globals.username + "'";
+            command = new OracleCommand(sql, conn);
+            command.Parameters.Add("Hoten", OracleDbType.NVarchar2, 40).Value = textBox_tenBN.Text;
+            command.Parameters.Add("SoCMND", OracleDbType.NVarchar2, 15).Value = textBox_CMND_BN.Text;
+            command.Parameters.Add("sonha", OracleDbType.NVarchar2, 10).Value = textBox_soNhaBN.Text;
+            command.Parameters.Add("tenduong", OracleDbType.NVarchar2, 40).Value = textBox_tenDuongBN.Text;
+            command.Parameters.Add("quanhuyen", OracleDbType.NVarchar2, 40).Value = textBox_quanHuyenBN.Text;
+            command.Parameters.Add("tinhtp", OracleDbType.NVarchar2, 40).Value = textBox_tinhThanhPho_BN.Text;
+
+            command.ExecuteNonQuery();
+            dgv_ThongTinBN.Update();
+            dgv_ThongTinBN.Refresh();
+            Load_Data();
+            conn.Close();
+        }
+
+        private void button_thoat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login_Form lg = new Login_Form();
+            lg.ShowDialog();
+            
+        }
+    }
+}
